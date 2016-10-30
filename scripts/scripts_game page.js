001dbf2cf158afa017ffa727e89a1d542027b0f8
Graham Burris <burrga01@ipfw.students.edu>
2016-10-30 18:57:46 -0400

    I made a java script file, and made a help button that explains the game

---
+++ scripts/scripts_game page.js
@@ -0,0 +1,13 @@
+/**
+ * Created by hamwa on 10/30/2016.
+ */
+
+
+function ShowHelp(){
+    document.getElementById("helpOutput").innerHTML="The objective of the game is to match a card number and color with the corresponding number and color." +
+        "For example, if you are to choose the two of clubs, you would match it with the two of spades.";
+
+}
+function ClearHelp(){
+    document.getElementById("helpOutput").innerHTML='';
+}
