12b04e3ae763348f615c77455e2b30f0c30cb8e4
Graham Burris <burrga01@ipfw.students.edu>
2016-11-09 17:20:29 -0500

    Worked further on the actual game, and I made some substantial logic progress. I think.

--- scripts/scripts_game page.js
+++ scripts/scripts_game page.js
@@ -1,7 +1,18 @@
 /**
  * Created by hamwa on 10/30/2016.
  */
+const CARD0 = "../Images2/red_joker.jpg";
+const CARD1 = "../Images2/2_of_clubs.jpg";
+const CARD2 = "../Images2/2_of_diamonds.jpg";
+const CARD3 = "../Images2/2_of_hearts.jpg";
+const CARD4 = "../Images2/2_of_spades.jpg";
 
+
+var TimesFlipped = 0;
+
+function $ (string){
+    document.getElementById("")
+}
 
 function ShowHelp(){
     document.getElementById("helpOutput").innerHTML="The objective of the game is to match a card number and color with the corresponding number and color." +
@@ -11,8 +22,56 @@ function ShowHelp(){
 function ClearHelp(){
     document.getElementById("helpOutput").innerHTML='';
 
-    document.getElementById('faceDownCard1').src = '2_of_clubs.png';
+
 }
 
 
+function FlipImage1(){
+    document.getElementById("image1").src = CARD1;
+    TimesFlipped ++;
+    alert(TimesFlipped);
 
+    if(TimesFlipped > 2){
+        document.getElementById("image1").src = CARD0;
+        document.getElementById("image2").src = CARD0;
+        document.getElementById("image3").src = CARD0;
+        document.getElementById("image4").src = CARD0;
+    }
+}
+
+function FlipImage2(){
+    document.getElementById("image2").src = CARD2;
+    TimesFlipped ++;
+
+    if(TimesFlipped > 2){
+        document.getElementById("image1").src = CARD0;
+        document.getElementById("image2").src = CARD0;
+        document.getElementById("image3").src = CARD0;
+        document.getElementById("image4").src = CARD0;
+    }
+
+    if(TimesFlipped = 2) setTimeout()
+}
+function FlipImage3(){
+    document.getElementById("image3").src = CARD3;
+    TimesFlipped ++;
+
+    if(TimesFlipped > 2){
+        document.getElementById("image1").src = CARD0;
+        document.getElementById("image2").src = CARD0;
+        document.getElementById("image3").src = CARD0;
+        document.getElementById("image4").src = CARD0;
+    }
+}
+function FlipImage4(){
+    document.getElementById("image4").src = CARD4;
+    TimesFlipped ++;
+    alert(TimesFlipped);
+
+    if(TimesFlipped > 2){
+        document.getElementById("image1").src = CARD0;
+        document.getElementById("image2").src = CARD0;
+        document.getElementById("image3").src = CARD0;
+        document.getElementById("image4").src = CARD0;
+    }
+}
